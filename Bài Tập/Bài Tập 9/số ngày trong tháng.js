2d4eb3d0fa6b6f278f2088bb1a00fd0c4f8362aa
Quanganh06 <lequanganhh1624@gmail.com>
2023-04-24 16:51:45 +0700

    ain

---
+++ Bài Tập/Bài Tập 9/số ngày trong tháng.js
@@ -0,0 +1,21 @@
+let Month = prompt("Nhập số tháng cần tìm ");
+if (Month>13)
+    alert("Yêu cầu nhập lại tháng ");
+switch (Month) {
+    case tháng 1:
+    case tháng 3:
+    case tháng 5:
+    case tháng 7:
+    case tháng 8:
+    case tháng 10:
+    case tháng 12:
+        text = "Tháng này có 31 ngày";
+        break;
+    case tháng 4:
+    case tháng 6:
+    case tháng 9:
+    case tháng 11:
+        text ="Tháng Này có 30 ngày";
+
+
+}
